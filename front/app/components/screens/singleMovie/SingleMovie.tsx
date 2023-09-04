c6fe9c9ed11e592404db0194122c0ba9f8d577a7
DanyaChupin <105223502+DanyaChupin@users.noreply.github.com>
2023-09-04 15:40:16 +0300

    Develop (#4)
    * Сделана страница:Trending, Freash.
    
    * Редактирование профиля и пользователей в админ панеле
    
    * Разработана страница фильма.
    
    * Разработан видео плеер

--- front/app/components/screens/singleMovie/SingleMovie.tsx
+++ front/app/components/screens/singleMovie/SingleMovie.tsx
@@ -6,6 +6,12 @@ import SubHeading from '@/components/ui/heading/SubHeading'
 import Gallery from '@/components/ui/gallery/Gallery'
 import Content from './Content/Content'
 
+import dynamic from 'next/dynamic'
+
+const DynamicVideoPlayer = dynamic(() => import('@/ui/videoPlayer/VideoPlayer'))
+
+
+
 const SingleMovie: FC<IMoviePage> = ({ movie, similarMovies }) => {
 	return (
 		<Meta title={movie?.title} description={`Watch ${movie.title}`}>
@@ -14,7 +20,11 @@ const SingleMovie: FC<IMoviePage> = ({ movie, similarMovies }) => {
 				Detail={() => <Content movie={movie} />}
 			/>
 
-			{/* video player */}
+
+			<DynamicVideoPlayer
+				slug={movie.slug}
+				videoSource={movie.videoUrl}
+			/>
 
 			<div className="mt-12">
 				<SubHeading title="Similar" />
